6c17e0aad10b9885c3f0154c297ab2c40cebae1a
ASmith-eng <alexsmith194@outlook.com>
2024-02-05 20:31:00 +0000

    refactor: index posts cleanup

--- hooks/indexPosts/errorHandling.js
+++ hooks/indexPosts/errorHandling.js
@@ -1,8 +1,19 @@
-class DetailedError extends Error {
-    constructor(...args) {
-        super(...args);
-        Error.captureStackTrace(this, DetailedError);
-    }
-}
+const throwDetailedError = (error) => {
+    if (error.code ==='ENOENT') {
+        console.log(error.message);
+        const fileNotFoundMessages = {
+            config: "Config file not found! make sure this is in the root of your project directory",
+            markdown: "It looks like you don't have a 'markdown' directory at /public/markdown"
+        };
 
-export default DetailedError;
+        Object.keys(fileNotFoundMessages).forEach((key) => {
+            if (error.message.includes(key)) {
+                console.log(fileNotFoundMessages[key]);
+                return;
+            }
+        });
+    }
+    throw new Error(error);
+};
+
+export default throwDetailedError;
--- hooks/indexPosts/indexPosts.js
+++ hooks/indexPosts/indexPosts.js
@@ -1,20 +1,30 @@
 import fs from 'fs';
 import path from 'path';
 import extractFrontMatter from '../../src/helpers/extractFrontMatter';
-import DetailedError from './errorHandling';
 
 const indexPosts = async () => {
-    const rootDir = path.join(__dirname, '..', '..');
-    const markdownFiles = fs.readdirSync(path.join(rootDir, 'public', 'markdown'));
-    const config = await JSON.parse(fs.readFileSync(path.join(rootDir, 'config.json')));
-    // todo: add error handling for if file does not exist
+    try {
+        const rootDir = path.join(__dirname, '..', '..');
+        const markdownDir = path.join(rootDir, 'public', 'markdown');
+        const dataDir = path.join(rootDir, 'src', 'data');
 
-    const posts = [];
+        if (!fs.existsSync(markdownDir)) {
+            console.log("Markdown directory not found, creating now...");
+            fs.mkdirSync(markdownDir);
+        }
+        if (!fs.existsSync(dataDir)) {
+            console.log("Data directory not found, creating now...");
+            fs.mkdirSync(dataDir);
+        }
 
-    markdownFiles.forEach((filename) => {
-        // Check the file type is markdown and is not empty before extracting metadata
-        // With extracted metadata object check for required keys before adding to posts
-        try {
+        const markdownFiles = fs.readdirSync(markdownDir);
+        const config = await JSON.parse(fs.readFileSync(path.join(rootDir, 'config.json')));
+
+        const posts = [];
+
+        markdownFiles.forEach((filename) => {
+            // Check the file type is markdown and is not empty before extracting metadata
+            // With extracted metadata object check for required keys before adding to posts
             if (path.extname(filename) ==='.md') {
                 const fileContent = fs.readFileSync(path.join(rootDir, 'public', 'markdown', filename));
                 if (fileContent.length > 0) {
@@ -26,46 +36,30 @@ const indexPosts = async () => {
                     }
                 }
             }
-        } catch (error) {
-            throw new Error(error);
-            // throw new DetailedError(error.name, error.message);
-            // Should throw error here to break out of vite build?
-        }
-    });
-    
-    if (posts.length) {
-        // Fetch list of unique categories
-        const categories = new Set(posts.map((post) => post.category));
-        categories.delete(undefined);
-        const categoriesJson = JSON.stringify([...categories]);
-        fs.writeFileSync(path.join(rootDir, 'src', 'data', 'postCategories.json'), categoriesJson, 'utf8');
-
-        categories.forEach((category) => {
-            const categoryPosts = posts.filter((post) => post.category === category);
-            fs.writeFileSync(path.join(rootDir, 'src', 'data', `${category}.json`), JSON.stringify(categoryPosts), 'utf8');
         });
+        
+        if (posts.length) {
+            // Fetch list of unique categories
+            const categories = new Set(posts.map((post) => post.category));
+            categories.delete(undefined);
+            const categoriesJson = JSON.stringify([...categories]);
+            fs.writeFileSync(path.join(dataDir, 'postCategories.json'), categoriesJson, 'utf8');
 
-        // Sort posts by most recent
-        posts.sort((a, b) => b.date-a.date);
-        const recentPosts = posts.slice(0, config.maxDisplayRecentPosts);
-        fs.writeFileSync(path.join(rootDir, 'src', 'data', 'recentPosts.json'), JSON.stringify(recentPosts), 'utf8');
+            categories.forEach((category) => {
+                const categoryPosts = posts.filter((post) => post.category === category);
+                fs.writeFileSync(path.join(dataDir, `${category}.json`), JSON.stringify(categoryPosts), 'utf8');
+            });
+
+            // Sort posts by most recent
+            posts.sort((a, b) => b.date-a.date);
+            const recentPosts = posts.slice(0, config.maxDisplayRecentPosts);
+            fs.writeFileSync(path.join(dataDir, 'recentPosts.json'), JSON.stringify(recentPosts), 'utf8');
+
+            // To do: tidy up use of config file - can run app in different modes?
+        }
+    } catch (error) {
+        throw new Error(error);
     }
-
-    // inputs: Markdown posts with frontmatter tags
-    // outputs: list of category tags, list of most recent posts, list of all posts' titles/descriptions
-
-    // check for:
-    // - posts with duplicate filenames/post titles
-
-    // Home
-    // List of categories
-    // Recent posts
-
-    // Category page
-    // List of posts in category
-
-    // Blog post
-    // Full md render
 };
 
 export default indexPosts;
