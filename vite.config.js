4890504e1f8d0f3782feb892fe7a8b491ef60281
ASmith-eng <alexsmith194@outlook.com>
2024-10-20 19:52:45 +0100

    wip: enforcing indexPosts synchronicity in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,31 @@ import react from '@vitejs/plugin-react';
 import indexPosts from './hooks/indexPosts/indexPosts';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, ssrBuild }) => {
-    return {
-      base: '/blog/',
-      plugins: [
-        {
-          name: 'build-index',
-          buildStart(options) {
-            // console.log(options);
-            indexPosts();
-            return;
-          }
-        },
-        react()
-      ],
-      build: {
-        outDir: 'dist',
-      },
-      publicDir: 'public',
-      server: {
-        watch: {
-          ignored: ['**/.env*']
-        }
+export default defineConfig(async ({ command, mode, ssrBuild }) => {
+  if (command === 'build') {
+    const indexResult = await indexPosts();
+  }
+  return {
+    base: '/blog/',
+    plugins: [
+      // {
+      //   name: 'build-index',
+      //   async buildStart(options) {
+      //     // console.log(options);
+      //     await indexPosts();
+      //     return;
+      //   }
+      // },
+      react()
+    ],
+    build: {
+      outDir: 'dist',
+    },
+    publicDir: 'public',
+    server: {
+      watch: {
+        ignored: ['**/.env*']
       }
     }
+  }
 })
